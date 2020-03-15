ca7bfa243ec075823edb18e326c548115b80e523
João Afonso Rios e Silva <re039847@resource.com.br>
2020-03-14 23:56:14 -0300

    Jpa repository, injeção de dependencia e database seeding

---
+++ src/main/java/com/qintess/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.qintess.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qintess.course.entidades.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
