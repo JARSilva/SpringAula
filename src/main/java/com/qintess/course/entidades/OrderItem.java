46fa80c5299e592380fbe911be1f45edad86f99e
João Afonso Rios e Silva <re039847@resource.com.br>
2020-03-16 11:48:08 -0300

    OrdemItem Parte 1

---
+++ src/main/java/com/qintess/course/entidades/OrderItem.java
@@ -0,0 +1,81 @@
+package com.qintess.course.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.qintess.course.entidades.pk.OrderItemPK;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer qtd;
+	private Double price;
+	
+	public OrderItem() {
+		super();
+	}
+	public OrderItem(Order order, Product product, Integer qtd, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.qtd = qtd;
+		this.price = price;
+	}
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	public Integer getQtd() {
+		return qtd;
+	}
+	public void setQtd(Integer qtd) {
+		this.qtd = qtd;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
