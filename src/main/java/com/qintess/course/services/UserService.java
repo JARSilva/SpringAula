46b46fadca907d4b09c11f774a34d9eed66156b2
João Afonso Rios e Silva <re039847@resource.com.br>
2020-03-16 23:43:58 -0300

    Tratamento da Exception no Update

--- src/main/java/com/qintess/course/services/UserService.java
+++ src/main/java/com/qintess/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.qintess.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,9 +41,13 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User user) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, user);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User user) {
 		entity.setName(user.getName());
